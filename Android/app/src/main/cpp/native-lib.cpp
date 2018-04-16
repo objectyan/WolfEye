7a72bab6d2928208448fa2633fca6e8a8a7b45e0
OY <objecttozero@sina.com>
2018-04-16 15:36:01 +0800

    add android project and app icon

---
+++ Android/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring
+
+JNICALL
+Java_me_objectyan_wolfeye_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
